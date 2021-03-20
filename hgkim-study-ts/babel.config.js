0b1c452e87f6ab98111f439b2dc334593f800bef
librekyu <khkhong1@gmail.com>
2021-03-20 23:39:42 +0900

    add webpack-cli

--- hgkim-study-ts/babel.config.js
+++ hgkim-study-ts/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function (api) {
-
   api.cache(true);
   return {
-    presets: [
-      '@babel/preset-typescript',
-    ],
+    presets: ['@babel/preset-typescript'],
     plugins: [
       [
         '@babel/plugin-transform-runtime',
--- hgkim-study-ts/webpack.config.js
+++ hgkim-study-ts/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production', /** minify */
-  target: 'node',
-  module: { /** loaders */
+  mode: 'production' /** minify */,
+  target: 'node' /** environment */,
+  module: {
+    /** loaders */
     rules: [
       {
         test: /\.tsx?$/,
@@ -15,12 +16,13 @@ module.exports = {
         include: path.join(__dirname),
         exclude: /(node_modules)|(dist)|(__tests__)/,
         use: 'babel-loader',
-      }
+      },
     ],
   },
-  resolve: { /** 모듈들이 리졸브 될 때, 웹팩 작동시 무슨 디렉토리를 봐야하는지 지정  */
-    modules: ['node_modules'], /** default, 모듈 탐색을 시작할 경로 지정 */
-    extensions: ['.tsx', '.ts', '.js'], /** 탐색할 모듈의 확장자를 지정 */
+  resolve: {
+    /** 모듈들이 리졸브 될 때, 웹팩 작동시 무슨 디렉토리를 봐야하는지 지정  */
+    modules: ['node_modules'] /** default, 모듈 탐색을 시작할 경로 지정 */,
+    extensions: ['.tsx', '.ts', '.js'] /** 탐색할 모듈의 확장자를 지정 */,
   },
   entry: './src/app.ts',
   output: {
