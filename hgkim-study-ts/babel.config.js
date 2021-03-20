b6db90ac8893633f67e86cb9ac50cf4792da8827
librekyu <khkhong1@gmail.com>
2021-03-20 23:08:12 +0900

    fix: dependecies

---
+++ hgkim-study-ts/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+
+  api.cache(true);
+  return {
+    presets: [
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          regenerator: true,
+          corejs: 3,
+        },
+      ],
+    ],
+  };
+};
--- hgkim-study-ts/webpack.config.js
+++ hgkim-study-ts/webpack.config.js
@@ -1,23 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.ts',
-  module: {
+  mode: 'production', /** minify */
+  target: 'node',
+  module: { /** loaders */
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules)|(dist)|(__tests__)/,
       },
+      {
+        test: [/\.ts?$/],
+        include: path.join(__dirname),
+        exclude: /(node_modules)|(dist)|(__tests__)/,
+        use: 'babel-loader',
+      }
     ],
   },
-  target: 'node',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+  resolve: { /** 모듈들이 리졸브 될 때, 웹팩 작동시 무슨 디렉토리를 봐야하는지 지정  */
+    modules: ['node_modules'], /** default, 모듈 탐색을 시작할 경로 지정 */
+    extensions: ['.tsx', '.ts', '.js'], /** 탐색할 모듈의 확장자를 지정 */
   },
+  entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production',
 };
