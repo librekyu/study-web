5abe2965770e54ce93c55d27906d42b36d1d8da7
Hongcheol Kim <hongcheolkim@Hongcheolui-MacBookPro.local>
2021-04-09 23:30:05 +0900

    fixing repo

---
+++ src/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var hostname = '127.0.0.1';
+var port = 3000;
+
+const server = http.createServer(
+    function(req, res) { 
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World\n'); 
+    }
+);
+
+server.listen(port, hostname);
+
+console.log('Server running at http://'+hostname+':'+port);
