1b6e4dd381e796f0d1c878ae3c9e1c386be61283
Hongcheol Kim <hongcheolkim@Hongcheolui-MacBookPro.local>
2021-04-09 23:30:05 +0900

    fixing repo

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import A, { componentB, word } from './index.copy.js';
+
+const asd = '';
+console.log(word);
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports={
+    mode: 'production', // or "development" or "none"
+    entry: {
+        testing:'./src/server.js'
+    },
+    target: 'node',
+    output: {
+        filename: '[name]_bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+      },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css'],
+    },
+};
