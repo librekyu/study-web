d51072c4c6a66dc12a59791c7aa181ebaff534a2
carminest <mymyhongz@naver.com>
2021-06-09 16:37:44 +0900

    useState useRef etc practice

--- next-boiler-plate_KHC/src/pages/Practice/App.tsx
+++ next-boiler-plate_KHC/src/pages/Practice/App.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
+import { setUseProxies } from 'immer';
 
 const App = () => {
-  const name = 'react';
+  const nextId = useRef(4);
+
+  const [inputs, setInputs] = useState({
+    //인풋값을 받기 위한 state 정의
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs; // input 값에 username, email을 넣어줌
+
+  const onChange = (e) => {
+    //input 값이 변할때 state값 갱신
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
   const style = {
     backgroundColor: 'black',
     color: 'white',
     fontSize: '30px',
   };
-  const users = [
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'name1',
@@ -28,7 +48,22 @@ const App = () => {
       username: 'name3',
       email: 'emaaaail@asd.com',
     },
-  ];
+  ]);
+  const onCreate = () => {
+    //onChange에서 갱신받은 state를 여기서 사용
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    //onCreate 함수로 input 값 초기화, nextId ref값 1 증가
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
 
   return (
     <div>
@@ -42,6 +77,13 @@ const App = () => {
       </Wrapper>
       <Counter />
       <InputSample />
+
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
     </div>
   );
